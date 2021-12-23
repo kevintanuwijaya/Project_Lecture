dc88c1730ea437cc0477562d59b00c8846a44199
mtirtapradja <mtirtapradja@gmail.com>
2021-12-23 13:19:35 +0700

    json encode test

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $categories = Http::get("https://bilocker.000webhostapp.com/BiLocker/GetItemCategories.php");
+    dd(json_decode($categories));
 });
