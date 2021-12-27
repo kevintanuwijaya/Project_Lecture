20dadfb0844ffe668f49b3af4a919d5e9e8f3797
mtirtapradja <mtirtapradja@gmail.com>
2021-12-27 10:28:08 +0700

    clean up

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -2,12 +2,10 @@
 
 @section('container')
     <h1>TEST</h1>
-    <!-- Masthead-->
     @include('partials.header')
-    <!-- Feature Section-->
     @include('partials.feature')
-    <!-- About Section-->
     @include('partials.about')
-    <!-- Contact Section-->
     @include('partials.contact')
+
+    @include('partials.footer')
 @endsection
