3c20c686b928b042f3650d582021169c3c585615
mtirtapradja <mtirtapradja@gmail.com>
2021-12-29 16:22:01 +0700

    get history and edit page

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -16,10 +16,10 @@ class MainController extends Controller
      */
     public function homePage()
     {
-
         $user = null;
         $transactions = null;
         $comments = json_decode(Http::get('https://bilocker.000webhostapp.com/BiLocker/GetUserComment.php'));
+        $histories = null;
 
         if (Cookie::get('remember') || Session::get('remember')) {
 
@@ -44,12 +44,14 @@ class MainController extends Controller
             $user->balance = $result->UserBalance;
 
             $transactions = $this->getCurrentTransaction($user->email);
+            $histories = $this->getHistory($user->email);
         }
 
         return view('layouts.home', [
             'user' => $user,
             'transactions' => $transactions,
             'comments' => $comments,
+            'histories' => $histories,
         ]);
     }
 
@@ -94,5 +96,51 @@ class MainController extends Controller
         if ($result) return $result->Transaction;
 
         return null;
+    }
+
+    public function getHistory($userEmail)
+    {
+        $response = Http::asForm()->post('https://bilocker.000webhostapp.com/BiLocker/History.php', [
+            'email' => $userEmail,
+        ]);
+
+        $result = json_decode($response);
+
+        if ($result) return $result->Transaction;
+
+        return null;
+    }
+
+    public function editPage()
+    {
+
+        if (Cookie::get('remember') || Session::get('remember')) {
+            $user = null;
+            $response = null;
+
+            if (Cookie::get('remember')) {
+                $response = Http::asForm()->post('https://bilocker.000webhostapp.com/BiLocker/GetUser.php', [
+                    'email' => Cookie::get('remember'),
+                ]);
+            } else {
+                $response = Http::asForm()->post('https://bilocker.000webhostapp.com/BiLocker/GetUser.php', [
+                    'email' => Session::get('remember'),
+                ]);
+            }
+
+            $result = json_decode($response);
+
+            $user = new User();
+            $user->name = $result->UserName;
+            $user->email = $result->UserEmail;
+            $user->password = $result->UserPassword;
+            $user->balance = $result->UserBalance;
+
+            return view('editProfile', [
+                'user' => $user,
+            ]);
+        }
+
+        return back();
     }
 }
